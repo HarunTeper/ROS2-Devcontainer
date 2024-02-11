57507e45f6552df5dfdab40c5be34967eb02a9bc
HarunTeper <harun.teper@tu-dortmund.de>
2024-02-11 20:40:45 +0100

    updated script and devcontainer

--- ros2_entrypoint.sh
+++ ros2_entrypoint.sh
@@ -6,9 +6,9 @@ vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos
 sudo apt -y upgrade
 sudo rosdep init
 rosdep update
-rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
+rosdep install --rosdistro humble --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
 colcon build --symlink-install
-echo "source /workspace/local/ros2_humble/install/setup.bash" >> ~/.bashrc
+echo ". /workspace/local/ros2_humble/install/setup.sh" >> ~/.bashrc
 
 # source ros2 humble and go to the workspace
 . /workspace/local/ros2_humble/install/setup.sh
